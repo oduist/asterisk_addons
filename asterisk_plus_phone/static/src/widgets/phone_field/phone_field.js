59e915b4fc023ef131520edd80582cb030bb23a7
Nicolae Postica <nicolae.postica2@gmail.com>
2025-11-03 01:42:16 +0200

    Fix originate for 16.0

--- asterisk_plus_phone/static/src/widgets/phone_field/phone_field.js
+++ asterisk_plus_phone/static/src/widgets/phone_field/phone_field.js
@@ -20,7 +20,7 @@ patch(PhoneField.prototype, "asterisk_plus_phone.PhoneField", {
             ["originate_type"]
         )
         if (this.mainPhone && asterisk_user && asterisk_user.originate_type === 'client') {
-            let props = {phone: this.props.record.data[this.props.name]}
+            let props = {phone: this.props.value}
             this.mainPhone.props.bus.trigger('busPhoneMakeCall', props)
         } else {
             const {resModel, data} = this.props.record
