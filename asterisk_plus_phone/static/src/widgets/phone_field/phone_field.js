bb87690612321e32655d605990ac49a97deec0fa
Nicolae Postica <nicolae.postica2@gmail.com>
2025-11-03 00:02:12 +0200

    Fix error if originate non user

--- asterisk_plus_phone/static/src/widgets/phone_field/phone_field.js
+++ asterisk_plus_phone/static/src/widgets/phone_field/phone_field.js
@@ -6,11 +6,10 @@ import {PhoneField} from "@web/views/fields/phone/phone_field"
 import {session} from "@web/session"
 import {registry} from "@web/core/registry"
 
-patch(PhoneField.prototype, {
+patch(PhoneField.prototype, "asterisk_plus_phone.PhoneField", {
     setup() {
-        super.setup()
-        this.messaging = null
-        this.mainPhone = registry.category("main_components").get('mainPhone')
+        this._super.apply()
+        this.mainPhone = registry.category("main_components").get('mainPhone', null)
     },
 
     async _onClickCallButton(e) {
@@ -20,7 +19,7 @@ patch(PhoneField.prototype, {
             [["user", "=", session.uid]],
             ["originate_type"]
         )
-        if (asterisk_user && asterisk_user.originate_type === 'client') {
+        if (this.mainPhone && asterisk_user && asterisk_user.originate_type === 'client') {
             let props = {phone: this.props.record.data[this.props.name]}
             this.mainPhone.props.bus.trigger('busPhoneMakeCall', props)
         } else {
